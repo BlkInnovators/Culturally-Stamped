c55f0c23c23c341d551be6ebf10c776a849fc4f4
Sydney Jenkins <sjenki18@bulldogs.aamu.edu>
2021-03-28 20:40:16 -0500

    Initial Commit

---
+++ Culturally-Stamped/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Culturally-Stamped
+//
+//  Created by Sydney Jenkins on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
